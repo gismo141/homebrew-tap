34f9c2c13ad2b2a6f57621c287812f561631847c
gismo141 <michael.r141@gmail.com>
2016-07-18 09:18:58 +0200

    Added brewup and delempty

---
+++ brewup.rb
@@ -0,0 +1,11 @@
+class Brewup < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/gismo141/brewup/archive/1.0.0.tar.gz"
+  sha256 "433dd4e94e5fdf786eef9a7ce780cf60ca1cb5a3a48f623785954cce6b86bc07"
+  version "1.0.0"
+
+  def install
+    bin.install "brewup"
+  end
+end
---
+++ delempty.rb
@@ -0,0 +1,11 @@
+class Delempty < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/gismo141/delempty/archive/1.0.0.tar.gz"
+  sha256 "433dd4e94e5fdf786eef9a7ce780cf60ca1cb5a3a48f623785954cce6b86bc07"
+  version "1.0.0"
+
+  def install
+    bin.install "delempty"
+  end
+end
