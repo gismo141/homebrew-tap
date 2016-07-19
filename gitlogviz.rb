95fe2629c10a47820b94b43d17e33dab3f15110b
gismo141 <michael.r141@gmail.com>
2016-07-19 13:44:54 +0200

    Corrected SHA

--- gitlogviz.rb
+++ gitlogviz.rb
@@ -2,7 +2,7 @@ class Gitlogviz < Formula
   desc ""
   homepage ""
   url "https://github.com/gismo141/gitlogviz/archive/1.0.1.tar.gz"
-  sha256 "5f415cea6eaca4d411397cd8c941e4267731599e"
+  sha256 "9c5d63294bc430251017f198d62f99057ea880fdceb417da20fac2bd02e7fde9"
   version "1.0.1"
 
   def install
