99bf75a367280b69aa1a37a89a8c00fd4344f778
gismo141 <michael.r141@gmail.com>
2016-07-19 07:51:19 +0200

    Added software to generate visual git-commit output

---
+++ gitlog.rb
@@ -0,0 +1,11 @@
+class Gitlog < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/gismo141/gitlogviz/archive/1.0.0.tar.gz"
+  sha256 "a87a33fd75124785828a684f38733e966e2cd62e1665967fd9129aa0cd491677"
+  version "1.0.0"
+
+  def install
+    bin.install "gitlogviz"
+  end
+end
