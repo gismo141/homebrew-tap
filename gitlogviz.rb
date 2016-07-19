a4a63a06dbfe699377a78bc16d199ecbef7d2896
gismo141 <michael.r141@gmail.com>
2016-07-19 13:37:44 +0200

    Updated to new version

--- gitlogviz.rb
+++ gitlogviz.rb
@@ -3,7 +3,7 @@ class Gitlogviz < Formula
   homepage ""
   url "https://github.com/gismo141/gitlogviz/archive/1.0.0.tar.gz"
   sha256 "8281d77395b0b0f7508b428c58ff4a8b5655dd67b4807e5a6fbce3d89a06c60f"
-  version "1.0.0"
+  version "1.0.1"
 
   def install
     bin.install "gitlogviz.rb"
